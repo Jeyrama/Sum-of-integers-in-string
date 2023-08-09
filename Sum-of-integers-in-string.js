c391e26e33585c9b02773ae1c9fc8df8a5be1278
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-08-08 21:03:05 -0700

    Update Sum-of-integers-in-string.js

--- Sum-of-integers-in-string.js
+++ Sum-of-integers-in-string.js
@@ -17,3 +17,21 @@ function sumOfIntegersInString(s) {
 }
 
 // or
+
+function sumOfIntegersInString(s){
+  const numbers = '1234567890'
+  let res = ''
+  let sum = 0
+    for (let i = 0; i <= s.length; i += 1) {
+      if (numbers.includes(s[i])) {
+        res += s[i]
+      }
+      else {
+        if (res !== '') {
+          sum = sum + parseInt(res)
+          res = ''
+        }
+      }
+    }
+    return sum
+  }
